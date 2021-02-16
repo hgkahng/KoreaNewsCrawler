387accd1da282409c8806924b4bb8a9abf099e63
Hyungu Kahng <hgkahng@korea.ac.kr>
2021-02-16 07:49:37 +0000

    Run sequentially

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 echo "Crawling..."
-YEAR=1996
+YEAR=2015
 python main.py \
     --categories 정치 \
     --start_year $YEAR \
     --start_month 1 \
     --end_year $YEAR \
-    --end_month 12
+    --end_month 6 \
+    --join
 echo "Finished."

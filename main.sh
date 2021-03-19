b0d20657be14447ba35df86c7638f8f6f5274b58
Hyungu Kahng <hgkahng@korea.ac.kr>
2021-03-19 18:08:17 +0900

    change defaults

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 echo "Crawling..."
-YEAR=2015
+YEAR=2020
 python main.py \
-    --categories 정치 \
+    --categories 정치 사회 경제 IT과학 \
     --start_year $YEAR \
     --start_month 1 \
     --end_year $YEAR \
-    --end_month 6 \
+    --end_month 12 \
     --join
 echo "Finished."
